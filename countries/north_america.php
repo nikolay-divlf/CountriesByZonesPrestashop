c83d43c02fc1994c0b9b796c5eff7bc60a6c8c66
nikolay-divlf <nikol73333333@yandex.ru>
2024-11-05 13:58:57 +0400

    first commit

---
+++ countries/north_america.php
@@ -0,0 +1,66 @@
+<?php
+
+$list = [
+    [
+        0 => [
+            'country_iso_code' => 'AS',
+            'country_call_prefix' => '0',
+            'country_name' => 'American Samoa',
+            'zone_name' => 'North America',
+        ],
+        1 => [
+            'country_iso_code' => 'AG',
+            'country_call_prefix' => '0',
+            'country_name' => 'Antigua and Barbuda',
+            'zone_name' => 'North America',
+        ],
+        2 => [
+            'country_iso_code' => 'BS',
+            'country_call_prefix' => '0',
+            'country_name' => 'Bahamas',
+            'zone_name' => 'North America',
+        ],
+        3 => [
+            'country_iso_code' => 'BB',
+            'country_call_prefix' => '0',
+            'country_name' => 'Barbados',
+            'zone_name' => 'North America',
+        ],
+        4 => [
+            'country_iso_code' => 'BM',
+            'country_call_prefix' => '0',
+            'country_name' => 'Bermuda',
+            'zone_name' => 'North America',
+        ],
+        5 => [
+            'country_iso_code' => 'CA',
+            'country_call_prefix' => '1',
+            'country_name' => 'Canada',
+            'zone_name' => 'North America',
+        ],
+        6 => [
+            'country_iso_code' => 'MX',
+            'country_call_prefix' => '52',
+            'country_name' => 'Mexico',
+            'zone_name' => 'North America',
+        ],
+        7 => [
+            'country_iso_code' => 'US',
+            'country_call_prefix' => '1',
+            'country_name' => 'United States',
+            'zone_name' => 'North America',
+        ],
+        8 => [
+            'country_iso_code' => 'VG',
+            'country_call_prefix' => '0',
+            'country_name' => 'Virgin Islands (British)',
+            'zone_name' => 'North America',
+        ],
+        9 => [
+            'country_iso_code' => 'VI',
+            'country_call_prefix' => '0',
+            'country_name' => 'Virgin Islands (U.S.)',
+            'zone_name' => 'North America',
+        ],
+    ],
+];
